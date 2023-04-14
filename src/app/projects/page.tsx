d29a872235c10417c1b77d28a8540ba8f8ce75a6
Rohit Tokala <rtokala@purdue.edu>
2023-04-14 14:20:46 -0400

    Add initial project cards and page layout

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,5 +1,42 @@
+import ProjectCard from "@/components/ProjectCard";
+import RibbitThumb from "../../../public/ribbit-thumb.webp";
+import SpredditThumb from "../../../public/spreddit-thumb.webp";
+
 function ProjectsPage() {
-  return <div>ProjectsPage</div>;
+  return (
+    <div className="min-h-screen flex pb-14 bg-base-300 gap-4 flex-col md:flex-row items-center justify-evenly w-full">
+      <ProjectCard
+        thumb={RibbitThumb}
+        title="Ribbit"
+        blurb="A fullstack Twitter-like web app with Auth"
+        tags={[
+          "Next.js",
+          "TailwindCSS",
+          "TypeScript",
+          "NextAuth",
+          "MySQL",
+          "tRPC",
+        ]}
+        gitLink="https://github.com/rtokala14/ribbit"
+        liveLink="https://ribbit-sooty.vercel.app/"
+      />
+      <ProjectCard
+        thumb={SpredditThumb}
+        title="Spreddit"
+        blurb="A fullstack Reddit-like web app with Auth"
+        tags={[
+          "Next.js",
+          "TailwindCSS",
+          "TypeScript",
+          "NextAuth",
+          "PostgreSQL",
+          "tRPC",
+        ]}
+        gitLink="https://github.com/rtokala14/spreddit-t3"
+        liveLink="https://spreddit-t3.vercel.app/"
+      />
+    </div>
+  );
 }
 
 export default ProjectsPage;
