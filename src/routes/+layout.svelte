c4d2d65678f717f07e083ecced3ba63ae173267d
Rohit Tokala <tokalarr@gmail.com>
2024-09-30 19:24:17 +0400

    Add flowbite-runes and initial navbar

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script>
+	import '../app.css';
+	import { Navbar, NavBrand, NavUl, NavLi } from 'svelte-5-ui-lib';
+
+	let { children } = $props();
+</script>
+
+<Navbar breakPoint="md" hamburgerMenu={true}>
+	{#snippet brand()}
+		<NavBrand siteName="Rohit Tokala"></NavBrand>
+	{/snippet}
+	<NavUl class="order-1">
+		<NavLi href="/">Home</NavLi>
+		<NavLi href="/components/navbar">Navbar</NavLi>
+		<NavLi href="/components/footer">Footer</NavLi>
+	</NavUl>
+</Navbar>
+{@render children()}
