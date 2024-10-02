f8bced05a1e803a84eacf8310a3e8975f79fcfb6
Rohit Tokala <tokalarr@gmail.com>
2024-10-02 14:33:24 +0400

    Add featured Projects

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,8 @@
 <script>
-	import { Button, Heading, P, Span } from 'svelte-5-ui-lib';
+	import { Button, Heading, P, Span, Card } from 'svelte-5-ui-lib';
 	import { ChevronRightOutline } from 'flowbite-svelte-icons';
 	import { fly } from 'svelte/transition';
+	import ProjectCard from '$lib/ProjectCard.svelte';
 
 	let introAni = $state(false);
 
@@ -56,5 +57,61 @@
 		<img src="/flask.png" alt="React Web Framework" />
 		<img src="/docker.png" alt="React Web Framework" />
 		<img src="/aws.png" alt="React Web Framework" />
+	</div>
+</div>
+
+<!-- Featured Projects -->
+<div class=" mb-16 flex w-full max-w-4xl flex-col items-center gap-4 px-6">
+	<h2 class=" text-3xl font-extrabold uppercase tracking-wider text-primary-600">
+		Featured Projects
+	</h2>
+	<div class=" grid w-full grid-cols-1 items-stretch gap-4 p-2 sm:grid-cols-2">
+		<ProjectCard
+			desc="A Website for a Restaurant in Dubai, fully optimized for SEO and for the web. Built using
+		Sveltekit, styled with TailwindCSS, and deployed Vercel."
+			title="HBH - Restaurant Website"
+			src="/hbh.webp"
+			alt="HBH Website Homepage"
+			href="https://hbhdubai.com"
+		/>
+		<ProjectCard
+			desc="A Website for a Restaurant in Dubai, fully optimized for SEO and for the web. Built using
+		Sveltekit, styled with TailwindCSS, and deployed Vercel."
+			title="HBH - Restaurant Website"
+			src="/hbh.webp"
+			alt="HBH Website Homepage"
+			href="https://hbhdubai.com"
+		/><ProjectCard
+			desc="A Website for a Restaurant in Dubai, fully optimized for SEO and for the web. Built using
+	Sveltekit, styled with TailwindCSS, and deployed Vercel."
+			title="HBH - Restaurant Website"
+			src="/hbh.webp"
+			alt="HBH Website Homepage"
+			gitLink="https://gitjsdfkjl"
+			href="https://hbhdubai.com"
+		/>
+		<ProjectCard
+			desc="A Website for a Restaurant in Dubai, fully optimized for SEO and for the web. Built using
+		Sveltekit, styled with TailwindCSS, and deployed Vercel."
+			title="HBH - Restaurant Website"
+			src="/hbh.webp"
+			alt="HBH Website Homepage"
+			href="https://hbhdubai.com"
+		/>
+	</div>
+
+	<div class="mt-6 flex justify-center">
+		<Card class="text-center" size="lg" color="teal" padding="xl">
+			<h5 class="mb-2 text-3xl font-bold text-gray-900 dark:text-white">Learn More</h5>
+			<p class="mb-5 text-base text-gray-500 dark:text-gray-400 sm:text-lg">
+				View all my projects, or learn more about me and my interests.
+			</p>
+			<div
+				class="items-center justify-center space-y-4 sm:flex sm:space-x-4 sm:space-y-0 rtl:space-x-reverse"
+			>
+				<Button color="dark" class=" w-36" size="lg" href="/projects">All Projects</Button>
+				<Button color="dark" class=" w-36" size="lg" href="/projects">About Me</Button>
+			</div>
+		</Card>
 	</div>
 </div>
